6d1abe0737869e96d8248ba80804229888bfb848
souljadev <brendan.moy@gmail.com>
2017-12-03 17:33:30 -0500

    Add pic to controller and serializer

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -45,6 +45,6 @@ class ClientsController < ProtectedController
 
   # Only allow a trusted parameter "white list" through.
   def client_params
-    params.require(:client).permit(:first_name, :last_name, :email)
+    params.require(:client).permit(:first_name, :last_name, :email, :pic)
   end
 end
--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,5 +1,5 @@
 class ClientSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :editable
+  attributes :id, :first_name, :last_name, :email, :editable, :pic
   def editable
     scope == object.user
   end
